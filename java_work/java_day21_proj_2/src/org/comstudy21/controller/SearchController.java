b568dd035fcbc5306f48717d11728f9861b63a85
jeongeun-cloud <dyden2ehd@naver.com>
2020-04-14 14:37:23 +0900

    json data CRUD

---
+++ java_work/java_day21_proj_2/src/org/comstudy21/controller/SearchController.java
@@ -0,0 +1,27 @@
+package org.comstudy21.controller;
+
+import java.util.Vector;
+
+import org.comstudy21.model.Member;
+
+public class SearchController implements Controller {
+
+	public SearchController() {
+		
+	}
+
+	@Override
+	public void service() {
+		String name = nameField.getText();
+		String email = emailField.getText();
+		String phone = phoneField.getText();
+		
+		Member member = new Member(0, name, email, phone);
+		
+		Vector<Vector> dataVector = dao.select(member);
+		
+		dm.setDataVector(dataVector, columnNames);
+
+	}
+
+}
