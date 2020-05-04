52738e8cb551a100277028bc9e3df9c526e78c21
jeongeun-cloud <dyden2ehd@naver.com>
2020-05-04 18:33:51 +0900

    chapter15 I/O

---
+++ workspace/ch15/src/example/BufferedOutputStreamEx1.java
@@ -0,0 +1,31 @@
+package example;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class BufferedOutputStreamEx1 {
+	
+	public static void main(String[] args) {
+		try {
+			FileOutputStream fos = new FileOutputStream("123.txt");
+			
+			// BufferedOutputStream의 버퍼 크기를 5로 한다.
+			BufferedOutputStream bos = new BufferedOutputStream(fos, 5); 
+			
+			// 파일 123.txt에 1부터 9까지 출력한다.
+			// 보조스트림만 닫아주면 된다.
+			for(int i='1'; i<='9'; i++) {
+				bos.write(i);
+			}
+			
+			bos.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
